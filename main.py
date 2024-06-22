0c03a32a438c076646d27a98247efb7b23ed83aa
Vanden <88347410+Vandennn@users.noreply.github.com>
2024-06-22 17:02:14 -0400

    Complete Revamp of code
    Completely removed the old selenium code which was slow and very annoying to use. Now uses requests and is much less resource intensive and can run in the background with no problems.

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-import time, requests, json
+import requests, json, re, random
+from bs4 import BeautifulSoup
 from cleantext import clean
+from urllib.parse import quote
+from pathvalidate import sanitize_filepath
 
-def selenium_script():
-	URL = "GROUPALLCLOTHESLINK"
-	IDs = []
-	names = []
-	IDsDict = {}
-	driver = webdriver.Chrome()
-	driver.implicitly_wait(6)
-	driver.get(URL)
-	mainSec = driver.find_element(By.CSS_SELECTOR, '.hlist.item-cards-stackable.ng-scope')
-	elementList = mainSec.find_elements(By.CLASS_NAME , 'item-card-container')
-	for x in elementList:
-		# Select the <a> tag within the div
 
-		link_element = x.get_attribute('href').split('/')
-		print(link_element)
-		xStr = x.text[0:x.text.find("By ")]
-		if len(link_element) == 1:
-			pass
-		else:
-			names.append(xStr)
-			num = link_element[4]
-			IDs.append(num)
-			IDsDict.update({num: xStr})
-	time.sleep(1)
-	for x in IDsDict:
-		driver.get("https://assetdelivery.roblox.com/v1/assetId/" + x)
-		mainSTR = driver.find_element(By.XPATH, "/html/body/pre").text
-		link = str(json.loads(mainSTR)["location"])
-		try:
-			pngID = str(requests.get(link).content).split('<url>http://www.roblox.com/asset/?id=')[1].split('</url>')[0]
-			driver.get("https://assetdelivery.roblox.com/v1/assetId/" + pngID)
-			mainSTR = driver.find_element(By.XPATH, "/html/body/pre").text
-			link = str(json.loads(mainSTR)["location"])
-			with open(f'output/{clean(IDsDict.get(x), no_emoji=True)}.png', 'wb') as f:
-				f.write(requests.get(link).content)
-		except IndexError:
-			print("not pants or shirt")
-		
-if __name__ == "__main__":
-	selenium_script()
+groupName = "TIX Clothing +"
+
+
+def scanner(url):
+	source = requests.get(url).text
+	linkList = [url]
+	urls = []
+	nPC = json.loads(source)["nextPageCursor"]
+	pattern = r"/?id=(.*?)</url>"
+
+	while nPC != None:
+		linkList.append(url + "&cursor=" + nPC)
+		nPC = json.loads(requests.get(linkList[len(linkList)-1]).text)["nextPageCursor"]
+		#print(nPC)
+	print(linkList)
+	for link in linkList:
+		print(json.loads(requests.get(link).text)['data'])
+	for link in linkList:
+		for x in json.loads(requests.get(link).text)['data']:
+			tempID = x['id']
+			#print(tempID)
+			mainUser = clean(sanitize_filepath(BeautifulSoup(requests.get("https://www.roblox.com/catalog/" + str(tempID)).text, 'html.parser').title.string.split('- Roblox')[0]),no_emoji=True,no_punct=True)
+			#print(requests.get(json.loads(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + str(x['id'])).text)['location']).text)
+			#print(requests.get(json.loads(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + str(x['id'])).text)['location']).text)
+			code = re.search(pattern, requests.get(json.loads(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + str(x['id'])).text)['location']).text).group(1)
+			#print(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + code).text)
+			with open('output/' + mainUser + '.png', 'wb') as f:
+				f.write(requests.get(json.loads(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + code).text)['location']).content)
+	return urls
+scanner("https://catalog.roblox.com/v1/search/items?category=Clothing&creatorName=" + quote(groupName) + "&creatorType=Group&limit=120&salesTypeFilter=1&subcategory=ClassicShirts")
