ce975cc95f77d6f1ec08c17c34bc2086a5ab9c64
Vanden <88347410+Vandennn@users.noreply.github.com>
2024-06-24 18:24:41 -0400

    added watermark remover to main.py

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ from bs4 import BeautifulSoup
 from cleantext import clean
 from urllib.parse import quote
 from pathvalidate import sanitize_filepath
+from PIL import Image
 
-
-groupName = "GROUP NAME"
-shirtsOnly = False
+groupName = "Aesthetic Aethers"
+shirtsOnly = True
 pantsOnly = True
+removeWatermark = True
+
+
+def imgSaver(name, code):
+	with open('output/' + name + '.png', 'wb') as f:
+		f.write(requests.get(json.loads(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + code).text)['location']).content)
+
+def watermarkRemover(name):
+	mainImg = Image.open(r"output/" + name + ".png")
+	template = Image.open(r"C:\Users\Vanden\Desktop\massacc\template.png") 
+	mainImg.paste(template, (0,0), mask = template)
+	mainImg.save("output/" + name + ".png")
 
 def scanner(url):
 	source = requests.get(url).text
@@ -22,8 +34,6 @@ def scanner(url):
 		#print(nPC)
 	#print(linkList)
 	for link in linkList:
-		print(json.loads(requests.get(link).text)['data'])
-	for link in linkList:
 		for x in json.loads(requests.get(link).text)['data']:
 			tempID = x['id']
 			#print(tempID)
@@ -32,9 +42,12 @@ def scanner(url):
 			#print(requests.get(json.loads(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + str(x['id'])).text)['location']).text)
 			code = re.search(pattern, requests.get(json.loads(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + str(x['id'])).text)['location']).text).group(1)
 			#print(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + code).text)
-			with open('output/' + mainUser + '.png', 'wb') as f:
-				f.write(requests.get(json.loads(requests.get("https://assetdelivery.roblox.com/v1/assetId/" + code).text)['location']).content)
-	return urls
+			if removeWatermark == True:
+				imgSaver(mainUser, code)
+				watermarkRemover(mainUser)
+			else:
+				imgSaver(mainUser, code)
+
 
 if shirtsOnly == True and pantsOnly == True:
 	scanner("https://catalog.roblox.com/v1/search/items?category=Clothing&creatorName=" + quote(groupName) + "&creatorType=Group&limit=120&salesTypeFilter=1&subcategory=ClassicShirts")
@@ -45,3 +58,4 @@ elif shirtsOnly == False:
 	scanner("https://catalog.roblox.com/v1/search/items?category=Clothing&creatorName=" + quote(groupName) + "&creatorType=Group&limit=120&salesTypeFilter=1&subcategory=ClassicPants")
 elif pantsOnly == False:
 	scanner("https://catalog.roblox.com/v1/search/items?category=Clothing&creatorName=" + quote(groupName) + "&creatorType=Group&limit=120&salesTypeFilter=1&subcategory=ClassicShirts")
+
